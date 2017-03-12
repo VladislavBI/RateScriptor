39fecf848e9c087958617f426da54b9720339648
wbayrakvlad@gmail.com <hammer12>
2017-03-12 10:13:11 +0200

    first deploy

---
+++ RateScriptor.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RateScriptor.UI.Startup))]
+namespace RateScriptor.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
